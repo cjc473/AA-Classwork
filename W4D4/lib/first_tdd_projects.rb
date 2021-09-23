ca46ae5d9459c7677edee13c1d18180b1bcf2d1c
Chris <chris.cheasty3@gmail.com>
2021-09-23 14:41:02 -0700

    Create two_sum and my transpose

--- W4D4/lib/first_tdd_projects.rb
+++ W4D4/lib/first_tdd_projects.rb
@@ -1,5 +1,21 @@
-def my_uniq(array)
-  count = Hash.new(0)
-  array.each { |ele| count[ele] += 1 }
-  count.keys
+class Array
+
+  def my_uniq
+    count = Hash.new(0)
+    self.each { |ele| count[ele] += 1 }
+    count.keys
+  end
+
+  def two_sum
+    new_arr = []
+    self.each_with_index do |ele, idx|
+      self.each_with_index do |ele2, idx2|
+        if idx2 > idx && ele + ele2 == 0
+          new_arr << [idx, idx2]
+        end
+      end
+    end
+    new_arr
+  end
+
 end
--- W4D4/spec/first_tdd_project_spec.rb
+++ W4D4/spec/first_tdd_project_spec.rb
@@ -3,12 +3,40 @@ require 'first_tdd_projects'
 describe "my_uniq" do
   let(:array) { [1, 2, 2, 2, 4] }
     it "should take in an array" do
-        expect{my_uniq(array)}.to_not raise_error
+        expect{array.my_uniq}.to_not raise_error
     end
 
   context "when there are duplicates" do
     it "remove duplicates" do
-      expect(my_uniq(array)).to eq([1, 2, 2, 2, 4].uniq)
+      expect(array.my_uniq).to eq([1, 2, 2, 2, 4].uniq)
     end
   end
+end
+
+describe "two_sum" do
+  let(:array) { [-1, 0, 2, -2, 1] }
+    it "should take in an array" do
+      expect{array.two_sum}.to_not raise_error
+    end
+
+  context "when two elements equal 0" do
+    it "adds their index to array" do
+      expect(array.two_sum).to eq([[0, 4], [2, 3]])
+    end
+  end
+
+describe "my_transpose" do
+  let(:array) { [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+  ]}
+    it "should take in a matrix" do 
+      expect{array.my_transpose}.to_not raise_error
+    end
+  end
+
+
+
+
 end
