e941185cf0eaed0c0c2e1e0c9f266cae826d4e52
Chris <chris.cheasty3@gmail.com>
2021-09-30 15:33:04 -0700

    Create initial associations

--- W5D4/associations_exercise/app/models/user.rb
+++ W5D4/associations_exercise/app/models/user.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+
+  has_many :enrolled_courses,
+    through: :enrollments,
+    source: :course
+
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :Enrollment
+
 end
