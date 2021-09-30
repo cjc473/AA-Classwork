cd31f25d589fdab02cdcd1a48b87ab241c3d37cc
Chris <chris.cheasty3@gmail.com>
2021-09-30 16:54:13 -0700

    Complete associations exercise

--- W5D4/associations_exercise/app/models/course.rb
+++ W5D4/associations_exercise/app/models/course.rb
@@ -15,4 +15,19 @@ class Course < ApplicationRecord
     primary_key: :id,
     foreign_key: :course_id,
     class_name: :Enrollment
+
+  has_one :prerequisite,
+    primary_key: :id,
+    foreign_key: :prereq_id,
+    class_name: :Course
+
+  belongs_to :instructor,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :User
+
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :user
+
 end
--- W5D4/associations_exercise/app/models/user.rb
+++ W5D4/associations_exercise/app/models/user.rb
@@ -8,11 +8,16 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+
+  has_many :course_taught,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :Course
  
   has_many :enrollments,
-  primary_key: :id,
-  foreign_key: :student_id,
-  class_name: :Enrollment
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :Enrollment
     
   has_many :enrolled_courses,
     through: :enrollments,
