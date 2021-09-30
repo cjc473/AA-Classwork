962842cc5be890f345f7c4556271772cfd203f03
Julian Erville <jerville@gmail.com>
2021-09-30 19:22:46 -0400

    Debug has_many_through wrong order

--- W5D4/associations_exercise/app/models/user.rb
+++ W5D4/associations_exercise/app/models/user.rb
@@ -8,14 +8,14 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
-
+ 
+  has_many :enrollments,
+  primary_key: :id,
+  foreign_key: :student_id,
+  class_name: :Enrollment
+    
   has_many :enrolled_courses,
     through: :enrollments,
     source: :course
-
-  has_many :enrollments,
-    primary_key: :id,
-    foreign_key: :student_id,
-    class_name: :Enrollment
 
 end
