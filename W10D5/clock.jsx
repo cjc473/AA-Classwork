2aaaee503ee5ccc7870d1ea39598c917c2e7eaaf
cjc473 <chris.cheasty3@gmail.com>
2021-11-05 16:38:57 -0700

    Add additional functions to clock

--- W10D5/clock.jsx
+++ W10D5/clock.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
 class Clock extends React.Component {
+  constructor() {
+    super();
+    this.state = { time: new Date() }
+  }
+
+  tick(){
+    this.setState( {time: new Date()} )
+  }
 
   render() {
     return (<h1>High Tech Clock</h1>)
+  }
+
+  componentWillUnmount() {
+    this.tick()
   }
 }
 
--- W10D5/widgets.jsx
+++ W10D5/widgets.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./root";
+import Clock from "./clock";
 
 
 document.addEventListener("DOMContentLoaded", () => {
