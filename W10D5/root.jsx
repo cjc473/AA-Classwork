86070cc9b039599ab77ad686074f8fae2f9ddc2b
cjc473 <chris.cheasty3@gmail.com>
2021-11-05 15:27:40 -0700

    Complete Setup

---
+++ W10D5/root.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+// const Root = () => {
+//   return (<div>hello!</div>)
+// }
+
+class Root extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (<div>hello!</div>)
+  }
+}
+
+export default Root;
