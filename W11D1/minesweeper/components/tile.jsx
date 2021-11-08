61c2568820c31897918820b090e11359da1b4b8c
your name here <your_email_hereroelliga@yahoo.com>
2021-11-08 15:15:26 -0800

    tile

--- W11D1/minesweeper/components/tile.jsx
+++ W11D1/minesweeper/components/tile.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import * as Minesweeper from '../minesweeper.js'
 
 class Tile extends React.Component{
     constructor(props) {
       super(props);
-
+      const currentTile = this.props.tile
+        this.state = {
+            explored: currentTile.explored,
+            flagged: currentTile.flagged,
+            bombed: currentTile.bombed,
+            display: "_"
+        }
     }
+
+tileStatus(){
+    const currentTile = this.props.tile
+    if(this.explored){
+        if(this.bombed){
+            this.display = 'b'
+        } else if(currentTile.adjacentBombCount() > 0){
+            this.display = currentTile.adjacentBombCount()
+        } else {
+            this.display = ''
+        }
+    } else if (this.flagged){
+        this.display = 'f'
+    } else {
+        this.display = ''
+    }
+}
 
     render() {
       return (
