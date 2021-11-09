cae565ddc14f993c15f3bec58f52f71489d1dc3f
cjc473 <chris.cheasty3@gmail.com>
2021-11-08 16:04:35 -0800

    Add methods for tile Component

--- W11D1/minesweeper/components/tile.jsx
+++ W11D1/minesweeper/components/tile.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import * as Minesweeper from '../minesweeper.js'
 
+
 class Tile extends React.Component{
     constructor(props) {
       super(props);
@@ -21,22 +22,37 @@ tileStatus(){
         } else if(currentTile.adjacentBombCount() > 0){
             this.display = currentTile.adjacentBombCount()
         } else {
-            this.display = ''
+            this.display = '_'
         }
     } else if (this.flagged){
         this.display = 'f'
     } else {
-        this.display = ''
+        this.display = '_'
     }
+    return this.display
 }
 
     render() {
+      const currentTile = this.props.tile;
       return (
-        <div>
-          <h1>T</h1>
+        // <h1>T</h1>
+        <div className="tile">
+          { this.tileStatus()}
         </div>
       )
     }
+
+    handleClick() {
+      const currentTile = this.props.tile
+      const revealed = (!(currentTile.flagged) && currentTile.explored)
+      if (revealed) {
+
+      }
+      this.props.updateGame
+    }
+
+    /// handle click should update if tile is unrevealed and unflagged.
+    /// if flagged, only can update by unflaggging
 }
 
 export default Tile;
