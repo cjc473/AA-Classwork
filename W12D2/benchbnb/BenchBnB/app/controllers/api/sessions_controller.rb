bf195acdad1dca6a7279c743f4e72ef906ec90a1
David Chan <dchaan1@gmail.com>
2021-11-16 15:10:23 -0800

    Added application controller methods and routes

--- W12D2/benchbnb/BenchBnB/app/controllers/api/sessions_controller.rb
+++ W12D2/benchbnb/BenchBnB/app/controllers/api/sessions_controller.rb
@@ -1,10 +1,17 @@
 class Api::SessionsController < ApplicationController
 
   def create
-
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user.nil?
+      render json: ['Invalid username/password'], status: 401
+    else 
+      login!(@user)
+      render 'api/users/show'
+    end
   end
 
   def destroy
-    
+    logout!
+    render json: {message: 'Logout Successful'}
   end
 end
